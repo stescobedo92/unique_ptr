40cdc9e8b76c776f41e08bdf6c0428cd0829b2dc
stescobedo92 <stescobedo.31@gmail.com>
2023-07-12 09:28:22 -0600

    Adding implementation for UniquePtr struct

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,47 @@ struct UniquePtr<T> {
     ptr: *mut T,
     deleter: Box<dyn FnMut(*mut T)>,
 }
+
+impl<T> UniquePtr<T> {
+    fn new(ptr: *mut T) -> Self {
+        Self {
+            ptr,
+            deleter: Box::new(DefaultDeleter::delete),
+        }
+    }
+
+    fn new_with_deleter(ptr: *mut T, deleter: impl FnMut(*mut T)) -> Self {
+        Self {
+            ptr,
+            deleter: Box::new(deleter),
+        }
+    }
+
+    fn drop(self) {
+        (self.deleter)(self.ptr);
+    }
+
+    fn reset(&mut self, ptr: *mut T) {
+        (self.deleter)(self.ptr);
+        self.ptr = ptr;
+    }
+
+    fn release(&mut self) -> *mut T {
+        let ptr = self.ptr;
+        self.ptr = std::ptr::null_mut();
+        ptr
+    }
+
+    fn swap(&mut self, other: &mut Self) {
+        std::mem::swap(self.ptr, other.ptr);
+        std::mem::swap(self.deleter, other.deleter);
+    }
+
+    fn get(&self) -> *mut T {
+        self.ptr
+    }
+
+    fn get_deleter(&self) -> &dyn FnMut(*mut T) {
+        &*self.deleter
+    }
+}
